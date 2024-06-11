662a01f54f0a03d0c741e790ec8cbd9b0e64d4bf
Lucas gardanne <lucas.gardanne@lws-hospitality.com>
2024-06-11 17:46:19 +0200

    fix copyright

--- src/components/copyright/Copyright.js
+++ src/components/copyright/Copyright.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Copyright= ({name, firstname, year}) => {
+const Copyright = ({ name, firstname, year }) => {
   return (
     <div className="bg-darkblue flex justify-center py-4">
-        <p className="text-white text-sm">Made with ❤ by {firstname} {name} - ©{year} {firstname} {name}. Tous droits réservés.</p>
+      <p className="text-white text-sm">
+        Made with <span style={{ color: 'red' }}>❤</span> by {firstname} {name} - ©{year} {firstname} {name}. Tous droits réservés.
+      </p>
     </div>
   );
 };
 
 export default Copyright;
+
